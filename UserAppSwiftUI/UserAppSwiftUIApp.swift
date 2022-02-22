14679e4392035009e3eca81c115238895cf9e0a1
Bodikov Kuat <kuat.bodikov@gmail.com>
2022-02-22 12:20:13 +0500

    Initial Commit

---
+++ UserAppSwiftUI/UserAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserAppSwiftUIApp.swift
+//  UserAppSwiftUI
+//
+//  Created by Kuat Bodikov on 22.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct UserAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
